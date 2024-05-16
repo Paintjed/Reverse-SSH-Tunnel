5a474c6ce6d7837ca621361cedf0cfa9f97bb8ae
jed <jed@paintjet.com>
2024-05-16 14:54:21 +0800

    [chore] first commit

---
+++ setup_reverse_tunnel.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+if [ -z "$SUDO_USER" ]; then
+  echo "$0 must be called from sudo. Try: 'sudo ${0}'"
+  exit 1
+fi
+
+echo "Creating file in $STARTUP_SERVICE_LOCATION for auto start"
+echo "Installing openssh-server ,autossh and sshpass"
+apt-get install openssh-server autossh sshpass
+
+read -p "Enter the ip address of server host: " SERVER_IP
+read -p "Enter the user name of server host: " SERVER_USERNAME
+read -s -p "Enter the password of server host: " SERVER_PASSWORD
+
+# 使用 ssh 嘗試連接
+if sshpass -p "$SERVER_PASSWORD" ssh "$SERVER_USERNAME@$SERVER_IP" true; then
+  echo "Login successful"
+else
+  echo "Login failed"
+  exit 1
+fi
+
+STARTUP_SERVICE="autossh-tunnel.service"
+STARTUP_SERVICE_LOCATION="/etc/systemd/system/$STARTUP_SERVICE"
+RSA_KEY_NAME="autossh"
+RSA_KEY_LOCATION="$HOME/.ssh"
+
+echo "Generate RSA Public key and Private key without password"
+ssh-keygen -t rsa -N "" -f $RSA_KEY_LOCATION/$RSA_KEY_NAME
+public_key_content=$(cat $RSA_KEY_LOCATION/$RSA_KEY_NAME.pub)
+
+sshpass -p "$SERVER_PASSWORD" ssh $SERVER_USERNAME@$SERVER_IP "echo ${SERVER_PASSWORD} | sudo -S lsof -i4 -iTCP -sTCP:LISTEN -P -n"
+echo "Enter port number that will be mapped in the server (Not duplicate the above port numbers):"
+read PORT_NUMBER
+
+echo "Enter the name of device"
+read DEVICE_NAME
+
+# Create an account specifically for using "lsof" to identify the port number associated with each device.
+# add_user_error=($(ssh $SERVER_USERNAME@$SERVER_IP "useradd $DEVICE_NAME -m -s /bin/rbash"))
+# if [ $? -eq 0 ]; then
+#   echo "$DEVICE_NAME added successfully."
+# else
+#   echo $result
+# fi
+
+remote_script=$(
+  cat <<EOF
+  #!/bin/bash
+  set -e
+  echo ${SERVER_PASSWORD} | sudo -S useradd ${DEVICE_NAME} -m -s /bin/rbash # Use sudo for user creation
+  echo ${SERVER_PASSWORD} | sudo -S mkdir /home/${DEVICE_NAME}/.ssh 
+  echo ${SERVER_PASSWORD} | sudo -S mkdir /home/${DEVICE_NAME}/bin
+  echo ${SERVER_PASSWORD} | sudo -S touch /home/${DEVICE_NAME}/.ssh/authorized_keys
+  echo ${SERVER_PASSWORD} | sudo -S tee -a /home/${DEVICE_NAME}/.ssh/authorized_keys >/dev/null <<< "$public_key_content" # Double quotes for variable expansion
+  echo ${SERVER_PASSWORD} | sudo -S chown -R ${DEVICE_NAME}:${DEVICE_NAME} /home/${DEVICE_NAME}/.ssh # Use sudo for ownership changes
+  echo ${SERVER_PASSWORD} | sudo -S chmod -R 700 /home/${DEVICE_NAME}/.ssh
+EOF
+)
+
+sshpass -p "$SERVER_PASSWORD" ssh $SERVER_USERNAME@$SERVER_IP 'bash -s' <<<"$remote_script"
+remote_script_exit_status=$?
+
+if [ $remote_script_exit_status -ne 0 ]; then
+  echo "remote_script execution failed."
+  exit 1
+fi
+
+echo "remote_script execution successfully."
+# ps x | grep ssh
+# autossh -f -o "ServerAliveInterval 20" -o "ServerAliveCountMax 3" -NR $PORT_NUMBER:localhost:22 -i $RSA_KEY_LOCATION/$RSA_KEY_NAME $DEVICE_NAME@$SERVER_IP
+# ps x | grep ssh
+
+tee $STARTUP_SERVICE_LOCATION <<EOF
+[Unit]
+Description=AutoSSH tunnel service
+After=network.target
+
+[Service]
+Environment="AUTOSSH_GATETIME=0"
+ExecStart=/usr/bin/autossh -o "ServerAliveInterval 60" -o "ServerAliveCountMax 5" -NR ${PORT_NUMBER}:localhost:22 -i $RSA_KEY_LOCATION/$RSA_KEY_NAME ${DEVICE_NAME}@${SERVER_IP}
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable $STARTUP_SERVICE
+systemctl start $STARTUP_SERVICE
+systemctl status $STARTUP_SERVICE
